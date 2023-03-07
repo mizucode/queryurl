d7f6bb18a20afb9bf8c2a074d1aa54ec0018fc8c
mizucode <mizurisysthensis3@gmail.com>
2023-03-07 14:55:14 +0700

    first commit

---
+++ dosen.php
@@ -0,0 +1,32 @@
+<div class="row mx-auto">
+          <div class="col-lg-3 col-6">
+            <div class="small-box bg-success">
+              <div class="inner">
+                <h3>7</h3>
+
+                <p>Jumlah dosen</p>
+              </div>
+              <div class="icon">
+                <i class="ion ion-stats-bars"></i>
+              </div>
+              <a href="#" class="small-box-footer"
+                >lihat data <i class="fas fa-arrow-circle-right"></i
+              ></a>
+            </div>
+          </div>
+          <div class="col-lg-3 col-6">
+            <div class="small-box bg-gradient-warning">
+              <div class="inner">
+                <h3>0</h3>
+
+                <p>Mahasiswa</p>
+              </div>
+              <div class="icon">
+                <i class="ion ion-stats-bars"></i>
+              </div>
+              <a href="#" class="small-box-footer"
+                >lihat data <i class="fas fa-arrow-circle-right"></i
+              ></a>
+            </div>
+          </div>
+        </div>
